2d3dc9e89f297c8157d2f56840a401f6a14a40dc
BelalAhmed4 <belal.ahmad.3119@gmail.com>
2024-11-22 02:23:47 +0200

    Adding Comment

--- src/shared/services/eventService.ts
+++ src/shared/services/eventService.ts
@@ -1,19 +1,18 @@
-import { Observable,Subject } from "rxjs";
+import { Observable, Subject } from 'rxjs';
 
-class Eventservice{
-  private subject=new Subject();
-  emit(eventname:string,payload:any){
-    
-    this.subject.next({eventname,payload})
-  }
+class Eventservice {
+    private subject = new Subject();
+    emit(eventname: string, payload: any) {
+        this.subject.next({ eventname, payload });
+    }
 
-  listen(eventname:string,callback:(event:any)=>void){
-    this.subject.asObservable().subscribe((nextObj:any)=>{
-
-      if(eventname==nextObj.eventname){
-        callback(nextObj.payload)
-      }
-    })
-  }
+    listen(eventname: string, callback: (event: any) => void) {
+        this.subject.asObservable().subscribe((nextObj: any) => {
+            if (eventname == nextObj.eventname) {
+                callback(nextObj.payload);
+            }
+        });
+    }
 }
-export default new Eventservice()
+export default new Eventservice();
+// End Service
